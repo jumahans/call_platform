6a36790cf72c145cb178cf77bda8f56ea2165a3f
jumahans <hjmuthoni@gmail.com>
2026-05-30 16:29:38 +0300

    Fix DATABASE_URL loading via decouple

--- config/settings.py
+++ config/settings.py
@@ -118,7 +118,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
+        default=config('DATABASE_URL', default=f'sqlite:///{BASE_DIR / "db.sqlite3"}'),
         conn_max_age=600,
     )
 }
